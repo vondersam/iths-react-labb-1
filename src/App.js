7f2846b8a7a0065a4e5a508edc0c76f7716d92b2
Samuel Rodriguez <samuelyret@gmail.com>
2023-12-20 20:54:42 +0100

    Connect to Open Library API.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function loadBooks() {
+    setLoading(true);
+    const results = await fetch(
+      'https://openlibrary.org/search.json?title=the+lord+of+the+rings'
+    );
+    const data = await results.json();
+    setBooks(data.docs);
+    setLoading(false);
+  }
+  useEffect(() => {
+    loadBooks();
+  }, []);
   return (
     <div className="App">
-      <h1>BookKeep</h1>
+      <h1>Book Keep</h1>
       <p>Keep track of your books</p>
+      {!loading && (
+        <ul>
+          {books.map((book) => (
+            <li>{book.title}</li>
+          ))}
+        </ul>
+      )}
+      {loading && <div>Loading please wait...</div>}
     </div>
   );
 }
