080f43cc3ef66f2f37c89691dba5b69393839a40
Samuel Rodriguez <samuelyret@gmail.com>
2023-12-21 12:36:27 +0100

    Add BookList and Book components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -8,7 +9,7 @@ function App() {
   async function loadBooks() {
     setLoading(true);
     const results = await fetch(
-      'https://openlibrary.org/search.json?title=the+lord+of+the+rings'
+      'https://openlibrary.org/search.json?title=Norwegian+Wood'
     );
     const data = await results.json();
     setBooks(data.docs);
@@ -21,13 +22,7 @@ function App() {
     <div className="App">
       <h1>Book Keep</h1>
       <p>Keep track of your books</p>
-      {!loading && (
-        <ul>
-          {books.map((book) => (
-            <li>{book.title}</li>
-          ))}
-        </ul>
-      )}
+      {!loading && <BookList books={books} />}
       {loading && <div>Loading please wait...</div>}
     </div>
   );
---
+++ src/components/Book.jsx
@@ -0,0 +1,11 @@
+function Book({ author, title }) {
+  return (
+    <li>
+      <div className="item">
+        {title} - {title}
+      </div>
+    </li>
+  );
+}
+
+export default Book;
